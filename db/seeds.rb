1cfa36764a34549506ba587465ddf454195a066f
Jorge Targino <jorgetargino.dc@gmail.com>
2023-05-11 10:57:48 -0300

    Adicionando seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Status.create(status_name: "SUBSCRIPTION_PURCHASED")
+Status.create(status_name: "SUBSCRIPTION_CANCELED")
+Status.create(status_name: "SUBSCRIPTION_RESTARTED")
