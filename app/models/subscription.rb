5b2fb7dd9f912be4a4a694e0c16b0ba917762ce3
Jorge Targino <jorgetargino.dc@gmail.com>
2023-05-12 11:39:43 -0300

    Adicionando callback na subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,10 @@
 class Subscription < ApplicationRecord
     belongs_to :user
     belongs_to :status
+
+    before_save :create_event_history
+
+    def create_event_history
+      EventHistory.create(subscription_id: self.id, status_type: self.status.status_name) if self.status_id_changed?
+    end
 end
