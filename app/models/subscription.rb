2f4147b67d7687c9b579d2f504f3e5b3f8546e5b
Jorge Targino <jorgetargino.dc@gmail.com>
2023-05-30 08:43:43 -0300

    Update subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,9 +2,9 @@ class Subscription < ApplicationRecord
     belongs_to :user
     belongs_to :status
 
-    before_save :create_event_history
+    before_save :update_event_history
 
-    def create_event_history
+    def update_event_history
       EventHistory.create(subscription_id: self.id, status_type: self.status.status_name) if self.status_id_changed?
     end
 end
